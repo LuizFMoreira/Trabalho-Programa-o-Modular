41c694a8e2a5f49108643c8e16f0773727786cf8
LuizFMoreira <bluizfernando720@gmail.com>
2025-06-08 13:08:18 -0300

    Atributos/metodos (Esqueleto)

---
+++ Codigo/Apresentacao.java
@@ -0,0 +1,13 @@
+package Codigo;
+
+public class Apresentacao {
+    //Todos os atributos até o momento;
+    private Projeto projeto;
+    private Banca banca;
+    private local Sala;
+    private dataHora LocalDateTime;
+
+    public void avaliar() {
+        // Lógica para avaliar a apresentação
+    }
+}
